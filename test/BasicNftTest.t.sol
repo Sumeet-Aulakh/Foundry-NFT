ffc62c8722242da7d4e54791ae96a8dfe7588813
Sumeet Singh Aulakh <sumeetbinjal@gmail.com>
2024-01-11 03:18:55 -0600

    Add BasicNftTest.sol for unit testing

---
+++ test/BasicNftTest.t.sol
@@ -0,0 +1,41 @@
+//SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.20;
+
+import {Test} from "forge-std/Test.sol";
+import {DeployBasicNft} from "../script/DeployBasicNft.s.sol";
+import {BasicNft} from "../src/BasicNft.sol";
+
+contract BasicNftTest is Test {
+    DeployBasicNft public deployer;
+    BasicNft public basicNft;
+    address public USER = makeAddr("user");
+    string public constant PUG =
+        "ipfs://bafybeig37ioir76s7mg5oobetncojcm3c3hxasyd4rvid4jqhy4gkaheg4/?filename=0-PUG.json";
+
+    function setUp() external {
+        deployer = new DeployBasicNft();
+        basicNft = deployer.run();
+    }
+
+    function testNameIsCorrect() public view {
+        string memory expectedName = "Doggie";
+        string memory actualName = basicNft.name();
+        // assert(expectedName == actualName);  <-- this doesn't work
+        assert(
+            keccak256(abi.encodePacked(expectedName)) ==
+                keccak256(abi.encodePacked(actualName))
+        );
+    }
+
+    function testCanMintAndHaveABalance() public {
+        vm.prank(USER);
+        basicNft.mintNft(PUG);
+
+        assert(basicNft.balanceOf(USER) == 1);
+        assert(
+            keccak256(abi.encodePacked(PUG)) ==
+                keccak256(abi.encodePacked(basicNft.tokenURI(0)))
+        );
+    }
+}
