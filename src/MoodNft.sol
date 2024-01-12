c7b2bf560577f8017eebb54d122353091737ff73
Sumeet Singh Aulakh <sumeetbinjal@gmail.com>
2024-01-12 05:03:31 -0600

    Add SVG files and MoodNft contract

---
+++ src/MoodNft.sol
@@ -0,0 +1,29 @@
+//SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.20;
+
+import {ERC721} from "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+
+contract MoodNft is ERC721 {
+    uint256 private s_tokenCounter;
+    string private s_sadSvg;
+    string private s_happySvg;
+
+    constructor(
+        string memory sadSvg,
+        string memory happySvg
+    ) ERC721("Mood Nft", "MN") {
+        s_tokenCounter = 0;
+        s_sadSvg = sadSvg;
+        s_happySvg = happySvg;
+    }
+
+    function mintNft() public {
+        _safeMint(msg.sender, s_tokenCounter);
+        s_tokenCounter++;
+    }
+
+    function tokenURI(
+        uint256 tokenId
+    ) public view override returns (string memory) {}
+}
