ee5f123e3f6f1d84e45935c9629099581ac7d3d4
Sumeet Singh Aulakh <sumeetbinjal@gmail.com>
2024-01-12 07:11:59 -0600

    Add mood-based image selection for NFTs

--- src/MoodNft.sol
+++ src/MoodNft.sol
@@ -3,27 +3,66 @@
 pragma solidity ^0.8.20;
 
 import {ERC721} from "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import {Base64} from "@openzeppelin/contracts/utils/Base64.sol";
 
 contract MoodNft is ERC721 {
     uint256 private s_tokenCounter;
-    string private s_sadSvg;
-    string private s_happySvg;
+    string private s_sadSvgImageUri;
+    string private s_happySvgImageUri;
+
+    enum Mood {
+        HAPPY,
+        SAD
+    }
+
+    mapping(uint256 => Mood) private s_tokenToMood;
 
     constructor(
-        string memory sadSvg,
-        string memory happySvg
+        string memory sadSvgImageUri,
+        string memory happySvgImageUri
     ) ERC721("Mood Nft", "MN") {
         s_tokenCounter = 0;
-        s_sadSvg = sadSvg;
-        s_happySvg = happySvg;
+        s_sadSvgImageUri = sadSvgImageUri;
+        s_happySvgImageUri = happySvgImageUri;
     }
 
     function mintNft() public {
         _safeMint(msg.sender, s_tokenCounter);
+        s_tokenToMood[s_tokenCounter] = Mood.HAPPY;
         s_tokenCounter++;
+    }
+
+    function _baseURI() internal pure override returns (string memory) {
+        return "data:application/json;base64,";
     }
 
     function tokenURI(
         uint256 tokenId
-    ) public view override returns (string memory) {}
+    ) public view override returns (string memory) {
+        string memory imageURI;
+        if (s_tokenToMood[tokenId] == Mood.HAPPY) {
+            imageURI = s_happySvgImageUri;
+        } else {
+            imageURI = s_sadSvgImageUri;
+        }
+
+        return
+            string(
+                abi.encodePacked(
+                    _baseURI(),
+                    Base64.encode(
+                        bytes(
+                            abi.encodePacked(
+                                '{name: "',
+                                name(),
+                                '", ',
+                                '"description": "An NFT that changes based on the mood of the owner", "attributes": "[{"trait_type":"moodiness", "value":100}], "image": "',
+                                imageURI,
+                                '"}'
+                            )
+                        )
+                    )
+                )
+            );
+    }
 }
